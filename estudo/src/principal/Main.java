65c1dd879ad7aa8aa38730e6300f9b428072e16b
João Alfredo <joaoalfredo32@gmail.com>
2022-08-18 11:06:11 -0300

    Estudando o Java com GIT dia 02

--- estudo/src/principal/Main.java
+++ estudo/src/principal/Main.java
@@ -3,7 +3,8 @@ package principal;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.print("Hello World");
+		int primeiraParcelaSoma = 0;
+		int segundaPaercelaSoma = 20;
 
 	}
 
