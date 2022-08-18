e9c4b3a7ae431a3dcdb0a04e2d20cee8eb4b0452
João Alfredo <joaoalfredo32@gmail.com>
2022-08-18 13:34:26 -0300

    Usando ternario no java

---
+++ estudo/src/principal/CondicaoIF.java
@@ -0,0 +1,39 @@
+package principal;
+
+import java.util.Scanner;
+
+public class CondicaoIF {
+	public static void main(String[] args) {
+		/*Estudo baseado nas video aulas de: Luiz Segundo Dev
+		 * https://www.youtube.com/channel/UC6AHsMB_L3EoocGssU2uJBQ
+		 */
+/*		
+		Scanner sc = new Scanner(System.in);
+		int idade = sc.nextInt();
+		
+		if (idade < 18) {
+			System.out.println("Você não pode entrar na balada");
+			
+		}else {
+			System.out.println("Você pode entrar na balada e tomar muita cachaça");
+		}
+*/
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Digite sua idade: ");
+		int idade = sc.nextInt();
+		System.out.println("Você bebe? (1) Sim (2) Não");
+		int bebe = sc.nextInt();
+		boolean validacao = (idade >= 18 && bebe == 1)?true:false; 
+		
+		if(validacao) {
+			System.out.println("Você tem mais de 18 anos e bebe");
+		}else {
+			System.out.println("Você tem menos de 18 anos");
+		}
+		
+		
+		
+	}
+
+}
--- estudo/src/principal/Main.java
+++ estudo/src/principal/Main.java
@@ -2,10 +2,22 @@ package principal;
 
 public class Main {
 
+	/*Estudo baseado nas video aulas de: Luiz Segundo Dev
+	 * https://www.youtube.com/channel/UC6AHsMB_L3EoocGssU2uJBQ
+	 */
 	public static void main(String[] args) {
-		int primeiraParcelaSoma = 0;
-		int segundaPaercelaSoma = 20;
+		int primeiraParcelaSoma = 23;
+		int segundaParcelaSoma = 20;
+		int soma = primeiraParcelaSoma + segundaParcelaSoma;
 
+		System.out.printf("A soma das parcelas %d" ,soma);
+	
+		/*String é um objeto
+		 * todo objeto é um tipo*/
+		String nome = "João Alfredo";
+		
+		System.out.println(nome);
+		
 	}
 
 }
